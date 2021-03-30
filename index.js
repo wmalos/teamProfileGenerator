d9222ed42353df9974887f8eaf5f4d83c1603b8e
Will <wmalos22@gmail.com>
2021-03-30 16:33:25 -0700

    added questions and requirements

---
+++ index.js
@@ -0,0 +1,45 @@
+const inquirer = require("inquirer")
+const jest = require("jest")
+const generateHtml = require("./src/generateHtml")
+
+
+const questions = [
+
+    {
+        type: 'input',
+        name: '',
+        message: '',
+    },
+
+    {
+        type: 'input',
+        name: '',
+        message: '',
+    },
+
+    {
+        type: 'input',
+        name: '',
+        message: '',
+    },
+
+    {
+        type: 'input',
+        name: '',
+        message: '',
+    },
+
+    {
+        type: 'input',
+        name: '',
+        message: '',
+    },
+
+    {
+        type: 'input',
+        name: '',
+        message: '',
+    },
+
+
+]
