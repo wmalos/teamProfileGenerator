c6e11660d906382208acb6571de372ddcd288c1a
Will <wmalos22@gmail.com>
2021-04-05 18:54:13 -0700

    working on questions

--- index.js
+++ index.js
@@ -8,60 +8,73 @@ const employees = [];
 function addEmployee() {
     inquirer.prompt([
 
-    {
-        type: 'input',
-        name: 'name',
-        message: 'Enter team member name'
-    },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter team member name'
+        },
 
-    {
-        type: 'list',
-        name: 'role',
-        message: 'Enter the employees role',
-        choices: ["Engineer", "Intern", "Manager"]
-    },
+        {
+            type: 'list',
+            name: 'role',
+            message: 'Enter the employees role',
+            choices: ["Engineer", "Intern", "Manager"]
+        },
 
-    {
-        type: 'input',
-        name: 'id',
-        message: 'Enter employee ID number',
-    },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Enter employee ID number',
+        },
 
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Enter employees email',
-    }])
-    .then(function({name, role, id, email}) {
-        if (role === "Engineer") {
-            
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter employees email',
         }
-    }
+    ])
+        .then(function ({ name, role, id, email }) {
+            let roleInfo = "";
+            if (role === "Engineer") {
+                roleInfo = "GitHub username";
+            } else if (role === "Intern") {
+                roleInfo = "school name";
+            } else {
+                roleInfo = "office number";
+            }
+            inquirer.prompt([
+
+            {
+                type: 'input',
+                name: "roleInfo",
+                message: `Enter team member's ${roleInfo}`,
+            },
+
+            {
+                type: 'list',
+                name: 'addMember',
+                message: 'Do you want to add another team member?',
+                choices: ["yes","no"]
+            },
+
+            then(function() {
+                if (moreMembers === "yes") {
+                    addEmployee();
+                } else {
+                    generateHtml();
+                }
+            })
+
+        }
 
 
-    {
-        type: 'input',
-        name: 'officeNumber',
-        message: 'Enter managers office number',
-    },
 
-    {
-        type: 'input',
-        name: 'github',
-        message: 'Enter github username',
-    },
 
-    {
-        type: 'input',
-        name: 'school',
-        message: 'Enter current school'
-    },
-
-    {
-        type: 'confirm',
-        name: 'addEmployee',
-        default: true,
-    },
+   // {
+    //type: 'confirm',
+    //    name: 'addEmployee',
+   //     default: true,
+    //},
 
     ])
 }
